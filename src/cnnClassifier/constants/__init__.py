ce98e08c66a42de4dce08b4a3e1225c6f87fce76
Dipesh Shome <C:/Users/DIPESH SHOME>
2023-08-17 15:02:44 +0600

    Prepare Base Model stage commleted

--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH=Path("F:\Diseases-Classification\config\config.yaml")
-PARAMS_FILE_PATH=Path("F:\Diseases-Classification\params.yaml")
+CONFIG_FILE_PATH=Path("config\config.yaml")
+PARAMS_FILE_PATH=Path("params.yaml")
