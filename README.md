a04570e2c73220d770384c641f0b3b4b57e522c9
Dipesh Shome <C:/Users/DIPESH SHOME>
2023-08-18 17:58:16 +0600

    dvc added

--- README.md
+++ README.md
@@ -11,3 +11,20 @@
 7.Update the pipeline <br>
 8.Update the main.py <br>
 9.Update the dvc.yaml for tracking mlops pipeline.<br>
+
+
+
+## Pipeline
+
+1. Stage 1: Data Ingestion stage.<br>
+2. Stage 2: Prepare Base Model stage<br>
+3. Stage 3: Training<br>
+4. Stage 4: Evaluation stage<br>
+
+
+## DVC Command
+
+1. dvc init.<br>
+2. dvc repro.<br>
+3. dvc dag.<br>
+
--- main.py
+++ main.py
@@ -26,16 +26,6 @@ except Exception as e:
     raise e
 
 
-STAGE_NAME = "Prepare Base Model stage"
-try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = PrepareBaseModelTrainingPipeline()
-    obj.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-    logger.exception(e)
-    raise e
-
 STAGE_NAME = "Training"
 try: 
    logger.info(f"*******************")
